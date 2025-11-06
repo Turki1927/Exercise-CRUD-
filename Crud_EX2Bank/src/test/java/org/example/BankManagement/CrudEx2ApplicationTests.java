6f53da5bc4b902f56b52d532dc3254f4e7d3211a
Turki1927 <turkialhrbi5923@gmail.com>
2025-11-07 01:03:37 +0300

    Add files via upload

---
+++ Crud_EX2Bank/src/test/java/org/example/BankManagement/CrudEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.BankManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEx2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
